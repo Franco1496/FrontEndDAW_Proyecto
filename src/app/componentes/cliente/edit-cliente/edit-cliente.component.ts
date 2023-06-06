7990f4ac0b91946cbdc82b0b469552ac938cee98
JaimeDarlin <jaimedarlin.sg@gmail.com>
2023-06-06 04:31:59 -0500

    agregar metodos en el edit-cliente.component.ts y modificar el edit-cliente.component.html

--- src/app/componentes/cliente/edit-cliente/edit-cliente.component.ts
+++ src/app/componentes/cliente/edit-cliente/edit-cliente.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Cliente } from 'src/app/modelos/Cliente';
+import { Ubigeo } from 'src/app/modelos/Ubigeo';
+import { ClienteService } from 'src/app/servicio/cliente/cliente.service';
+import { UbigeoService } from 'src/app/servicio/ubigeo/ubigeo.service';
 
 @Component({
   selector: 'app-edit-cliente',
   templateUrl: './edit-cliente.component.html',
   styleUrls: ['./edit-cliente.component.css']
 })
-export class EditClienteComponent implements OnInit {
+export class EditClienteComponent implements OnInit{
 
-  constructor() { }
+  ubigeos?: Observable<Ubigeo[]>;
 
-  ngOnInit(): void {
+  cliente: Cliente = new Cliente();
+
+  constructor(private router:Router, private clienteService:ClienteService, private ubigeoService:UbigeoService){}
+
+  ngOnInit():void{
+    this.editar();
+    this.ubigeos = this.ubigeoService.getUbigeoList();
+  }
+
+  editar(){
+    let id_cliente = JSON.parse(localStorage.getItem('id_cliente') as string);
+    this.clienteService.getClienteId(id_cliente).subscribe(data=>{
+      this.cliente=data;
+    });
+  }
+
+  actualizar(cliente:Cliente){
+    this.clienteService.updateCliente(cliente).subscribe(data=>{
+      this.cliente=data;
+      this.router.navigate(['clientes']);
+    })
   }
 
 }
