b21ac002a4e57df68ca0c560b054cdf1cc26b904
JaimeDarlin <jaimedarlin.sg@gmail.com>
2023-06-06 04:39:33 -0500

    agregar metodos en el edit-propiedad.component.ts y modificar el add-propiedad.component.html

--- src/app/componentes/propiedad/edit-propiedad/edit-propiedad.component.ts
+++ src/app/componentes/propiedad/edit-propiedad/edit-propiedad.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Propiedad } from 'src/app/modelos/Propiedad';
+import { TipoPropiedad } from 'src/app/modelos/TipoPropiedad';
+import { Ubigeo } from 'src/app/modelos/Ubigeo';
+import { UnidadMedida } from 'src/app/modelos/UnidadMedida';
+import { PropiedadService } from 'src/app/servicio/propiedad/propiedad.service';
+import { TipoPropiedadService } from 'src/app/servicio/tipoPropiedad/tipo-propiedad.service';
+import { UbigeoService } from 'src/app/servicio/ubigeo/ubigeo.service';
+import { UnidadMedidaService } from 'src/app/servicio/unidadMedida/unidad-medida.service';
 
 @Component({
   selector: 'app-edit-propiedad',
   templateUrl: './edit-propiedad.component.html',
   styleUrls: ['./edit-propiedad.component.css']
 })
-export class EditPropiedadComponent implements OnInit {
+export class EditPropiedadComponent implements OnInit{
 
-  constructor() { }
+  ubigeos?: Observable<Ubigeo[]>;
 
-  ngOnInit(): void {
+  tipo?: Observable<TipoPropiedad[]>;
+
+  medida?: Observable<UnidadMedida[]>;
+
+  propiedad: Propiedad = new Propiedad();
+
+  constructor(private router:Router,
+    private propiedadService:PropiedadService, private ubigeoService:UbigeoService,
+    private tipoPropiedadService:TipoPropiedadService, private unidadMedidaService:UnidadMedidaService){}
+
+  ngOnInit():void{
+    this.editar();
+    this.ubigeos = this.ubigeoService.getUbigeoList();
+    this.tipo = this.tipoPropiedadService.getTipoPropiedadList();
+    this.medida = this.unidadMedidaService.getUnidadMedidaList();
   }
 
+  editar(){
+    let idPropiedad = JSON.parse(localStorage.getItem('idPropiedad') as string);
+    this.propiedadService.getPropiedadId(idPropiedad).subscribe(data=>{
+      this.propiedad=data;
+    });
+  }
+
+  actualizar(propiedad:Propiedad){
+    this.propiedadService.updatePropiedad(propiedad).subscribe(data=>{
+      this.propiedad=data;
+      this.router.navigate(['propiedades']);
+    })
+  }
 }
