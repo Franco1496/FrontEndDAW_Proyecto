0d0a97642f9531699032c631302911c6260a4cec
Franco1496 <105532498+Franco1496@users.noreply.github.com>
2023-06-25 14:47:16 -0500

    servicio agregador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,24 @@ import { AddPropiedadComponent } from './componentes/propiedad/add-propiedad/add
 import { EditPropiedadComponent } from './componentes/propiedad/edit-propiedad/edit-propiedad.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AddAsesoresComponent } from './componentes/asesores/add-asesores/add-asesores.component';
+import { EditAsesoresComponent } from './componentes/asesores/edit-asesores/edit-asesores.component';
+import { ListarAsesoresComponent } from './componentes/asesores/listar-asesores/listar-asesores.component';
+import { AddCitasComponent } from './componentes/citas/add-citas/add-citas.component';
+import { EditCitasComponent } from './componentes/citas/edit-citas/edit-citas.component';
+import { ListarCitasComponent } from './componentes/citas/listar-citas/listar-citas.component';
+import { AddVentasComponent } from './componentes/ventas/add-ventas/add-ventas.component';
+import { EditVentasComponent } from './componentes/ventas/edit-ventas/edit-ventas.component';
+import { ListarVentasComponent } from './componentes/ventas/listar-ventas/listar-ventas.component';
+import { AddAsesorComponent } from './componentes/asesores/add-asesor/add-asesor.component';
+import { EditAsesorComponent } from './componentes/asesor/edit-asesor/edit-asesor.component';
+import { ListarAsesorComponent } from './componentes/asesor/listar-asesor/listar-asesor.component';
+import { AddCitaComponent } from './componentes/cita/add-cita/add-cita.component';
+import { EditCitaComponent } from './componentes/cita/edit-cita/edit-cita.component';
+import { ListarCitaComponent } from './componentes/cita/listar-cita/listar-cita.component';
+import { AddVentaComponent } from './componentes/venta/add-venta/add-venta.component';
+import { EditVentaComponent } from './componentes/venta/edit-venta/edit-venta.component';
+import { ListarVentaComponent } from './componentes/venta/listar-venta/listar-venta.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +38,25 @@ import { HttpClientModule } from '@angular/common/http';
     EditClienteComponent,
     ListarPropiedadComponent,
     AddPropiedadComponent,
-    EditPropiedadComponent
+    EditPropiedadComponent,
+    AddAsesoresComponent,
+    EditAsesoresComponent,
+    ListarAsesoresComponent,
+    AddCitasComponent,
+    EditCitasComponent,
+    ListarCitasComponent,
+    AddVentasComponent,
+    EditVentasComponent,
+    ListarVentasComponent,
+    AddAsesorComponent,
+    EditAsesorComponent,
+    ListarAsesorComponent,
+    AddCitaComponent,
+    EditCitaComponent,
+    ListarCitaComponent,
+    AddVentaComponent,
+    EditVentaComponent,
+    ListarVentaComponent
   ],
   imports: [
     BrowserModule,
