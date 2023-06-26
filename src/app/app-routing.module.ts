bc5bdcc7c313c4a689588b7c3009cc4f8769fc62
Franco1496 <105532498+Franco1496@users.noreply.github.com>
2023-06-25 21:39:24 -0500

    listado listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,15 @@ import { EditClienteComponent } from './componentes/cliente/edit-cliente/edit-cl
 import { ListarPropiedadComponent } from './componentes/propiedad/listar-propiedad/listar-propiedad.component';
 import { AddPropiedadComponent } from './componentes/propiedad/add-propiedad/add-propiedad.component';
 import { EditPropiedadComponent } from './componentes/propiedad/edit-propiedad/edit-propiedad.component';
+import { ListarAsesorComponent } from './componentes/asesor/listar-asesor/listar-asesor.component';
+import { AddAsesorComponent } from './componentes/asesor/add-asesor/add-asesor.component';
+import { EditAsesorComponent } from './componentes/asesor/edit-asesor/edit-asesor.component';
+import { ListarCitaComponent } from './componentes/cita/listar-cita/listar-cita.component';
+import { AddCitaComponent } from './componentes/cita/add-cita/add-cita.component';
+import { EditCitaComponent } from './componentes/cita/edit-cita/edit-cita.component';
+import { ListarVentaComponent } from './componentes/venta/listar-venta/listar-venta.component';
+import { AddVentaComponent } from './componentes/venta/add-venta/add-venta.component';
+import { EditVentaComponent } from './componentes/venta/edit-venta/edit-venta.component';
 
 const routes: Routes = [
   {path: "clientes", component:ListarClienteComponent},
@@ -14,7 +23,19 @@ const routes: Routes = [
 
   {path: "propiedades", component:ListarPropiedadComponent},
   {path: "nuevaPropiedad", component:AddPropiedadComponent},
-  {path: "editarPropiedad", component:EditPropiedadComponent}
+  {path: "editarPropiedad", component:EditPropiedadComponent},
+
+  {path: "asesores" , component:ListarAsesorComponent},
+  {path: "nuevoAsesor" , component:AddAsesorComponent},
+  {path: "editarAsesor" , component:EditAsesorComponent},
+
+  {path: "citas" , component:ListarCitaComponent },
+  {path: "nuevaCita" , component: AddCitaComponent},
+  {path: "editarCita" , component: EditCitaComponent},
+
+  {path: "ventas" , component: ListarVentaComponent},
+  {path: "nuevaVenta" , component: AddVentaComponent},
+  {path: "editarVenta" , component: EditVentaComponent}
   
 ];
 
--- src/app/servicio/asesor/asesor.service.ts
+++ src/app/servicio/asesor/asesor.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Asesor } from 'src/app/modelos/Asesor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AsesorService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  url = 'http://localhost:8080/api';
+
+
+
+  getAsesores(){
+    return this.http.get<Asesor[]>(this.url+"/asesor/");
+  }
+
+  createAsesor(asesor : Asesor){
+    return this.http.post<Asesor>(this.url+"/asesor/",asesor);
+  }
+
+  getAsesorId(id_asesor: number){
+    return this.http.get<Asesor>(this.url+"/asesor/"+id_asesor);
+  }
+
+  updateAsesor(asesor: Asesor){
+    return this.http.put<Asesor>(this.url+"/asesor/",asesor);
+  }
+
+  deleteAsesor(asesor: Asesor){
+    return this.http.delete<Asesor>(this.url+"/asesor/"+asesor.id_asesor);
+  }
+
 }
