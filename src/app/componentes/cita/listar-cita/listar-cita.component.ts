4ba072b753e5096ba6ae1e5bf65d69e04abcf7c9
Franco1496 <105532498+Franco1496@users.noreply.github.com>
2023-06-26 02:50:10 -0500

    ultimo

--- src/app/componentes/cita/listar-cita/listar-cita.component.ts
+++ src/app/componentes/cita/listar-cita/listar-cita.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Cita } from 'src/app/modelos/Cita';
+import { CargarScriptsService } from 'src/app/servicio/cargar-scripts.service';
+import { CitaService } from 'src/app/servicio/cita/cita.service';
+import { PropiedadService } from 'src/app/servicio/propiedad/propiedad.service';
 
 @Component({
   selector: 'app-listar-cita',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarCitaComponent implements OnInit {
 
-  constructor() { }
+  citas?:Cita[];
+
+  constructor(private citaService:CitaService, private _CargarScripts: CargarScriptsService, private router: Router){
+
+   }
 
   ngOnInit(): void {
+
+    this._CargarScripts.Carga(["crud/table"])
+
+    this.citaService.getCitas().subscribe(
+      data=>{
+        this.citas=data;
+        console.log(data);
+      },
+      error=>{
+        console.log(error);
+      }
+    );
+
+  }
+
+
+  nuevo():void{
+    this.router.navigate(['nuevaCita']);
+  }
+
+  editar(cita:Cita):void{
+    localStorage.setItem("id_cita",cita.id_cita.toString());
+    this.router.navigate(['editarCita']);
+  }
+
+  eliminar(cita:Cita):void{
+    this.citaService.deleteCita(cita).subscribe(data=>{
+      this.citas=this.citas!.filter(p=>p!==cita);
+    })
   }
 
 }
