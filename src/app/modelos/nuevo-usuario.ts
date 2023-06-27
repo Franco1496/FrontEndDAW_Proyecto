ac10099548ae1a19810d14da313e2958c80d0d91
ebrp-c <ebrp.1290@gmail.com>
2023-06-27 06:38:59 -0500

    se agrego model, service y componentes

---
+++ src/app/modelos/nuevo-usuario.ts
@@ -0,0 +1,10 @@
+export class NuevoUsuario {
+    nombre!: string;
+    apellido!: string;
+    telefono!: string;
+    nombreUsuario!: string;
+    email!: string;
+    password!: string;
+    authorities!: string[];
+
+}
